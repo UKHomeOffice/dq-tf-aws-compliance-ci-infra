e79bb4e44f805267b074ad79eeab2f0045c4caed
JWanderema <wanderema@hotmail.co.uk>
2021-03-09 17:38:44 +0000

    add bucket

---
+++ s3.tf
@@ -0,0 +1,55 @@
+resource "aws_s3_bucket" "dq_aws_config_bucket" {
+  bucket = "s3-dq-aws-config-ci"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = "arn:aws:s3:::dq-logs-archive/*"
+    target_prefix = "dq_aws_config/"
+  }
+
+  tags = {
+    Name = "s3-dq-aws-config-ci"
+  }
+}
+
+resource "aws_s3_bucket_policy" "dq_aws_config_bucket_policy" {
+  bucket = aws_s3_bucket.dq_aws_config_bucket
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "HTTP",
+      "Effect": "Deny",
+      "Principal": "*",
+      "Action": "*",
+      "Resource": "arn:aws:s3:::dq-logs-archive/*",
+      "Condition": {
+        "Bool": {
+          "aws:SecureTransport": "false"
+        }
+      }
+    }
+  ]
+}
+POLICY
+
+}
+
+resource "aws_s3_bucket_metric" "dq_aws_config_bucket_logging" {
+  bucket = aws_s3_bucket.dq_aws_config_bucket
+  name   = "dq_aws_config_metric"
+}
